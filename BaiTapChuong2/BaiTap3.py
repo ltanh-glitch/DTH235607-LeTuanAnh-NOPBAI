026219eeaab8a95a64a01e76b37bd3e219b8a138
ltanh2701-glitch <anh_dth235607@student.agu.edu.vn>
2025-08-29 22:33:52 +0700

    update

--- BaiTapChuong2/BaiTap3.py
+++ BaiTapChuong2/BaiTap3.py
@@ -6,12 +6,12 @@ dtb=(toan+ly+hoa)/3
 print("Điểm trung bình=",dtb)
 print("Điểm trung bình=",round(dtb,2))
 
-# Cách 2
-#print("Chương trình tính điểm trung bình")
-#toan,ly,hoa=eval(input("Nhập điểm toán,lý,hóa:"))
-#print("Điểm toán=",toan)
-#print("Điểm lý=",ly)
-#print("Điểm hóa=",hoa)
-#dtb=(toan+ly+hoa)/3
-#print("Điểm trung bình=",dtb)
-#print("Điểm làm tròn=",round(dtb,2))
+'''#Cách 2
+print("Chương trình tính điểm trung bình")
+toan,ly,hoa=eval(input("Nhập điểm toán,lý,hóa:"))
+print("Điểm toán=",toan)
+print("Điểm lý=",ly)
+print("Điểm hóa=",hoa)
+dtb=(toan+ly+hoa)/3
+print("Điểm trung bình=",dtb)
+print("Điểm làm tròn=",round(dtb,2))'''
